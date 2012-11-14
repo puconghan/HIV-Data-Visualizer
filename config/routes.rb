03f880f35cdde5f9bb968995592f2207cc15e3a3
Pucong Han <ph2369@columbia.edu>
2012-11-13 22:14:29 -0500

    Fixed all user validation/ Now need to work on the Admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 HIVDataFetcher::Application.routes.draw do
-  devise_for :users
-
+  devise_for :users do
+  	get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
   resources :users
   resources :hivdatafetcher
 
