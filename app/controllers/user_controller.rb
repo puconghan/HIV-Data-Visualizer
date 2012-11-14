f31d1272dfc18e431ca64819cc9c514ab17a886e
Pucong Han <ph2369@columbia.edu>
2012-11-14 01:06:24 -0500

    new controller for authenticate user 106

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def show
+    @userProfile = User.find(params[:id])
+    @user = current_user
+    @title = @userProfile.name
+  end
+end
